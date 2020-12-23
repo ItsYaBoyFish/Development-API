f8f15fca61d8d013c8283a0f7dd3cdd3b86272c8
Austin Graves <austingraves106@aol.com>
2020-12-22 20:46:33 -0600

    I forgot to add cors

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 const data = require('./data');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/users', (req, res) => {
   res.send(data);
